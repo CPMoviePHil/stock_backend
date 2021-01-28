5961c8c115c624a6c5de39b4c80335ecb0f0434a
big cock <0424002@nkust.edu.tw>
2021-01-28 20:45:34 +0800

    crawler_app

--- crawler_app/models.py
+++ crawler_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class PchStock(models.Model):
+    stock_name = models.CharField(max_length=255)
+    buy_price = models.IntegerField(default=0)
+    sell_price = models.IntegerField(default=0)
+    average_buy_price = models.IntegerField(default=0)
+    average_sell_price = models.IntegerField(default=0)
+    net_buy = models.IntegerField(default=0)
