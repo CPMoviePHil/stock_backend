4052a4674cb0037bf28cca3bb15835d3e5f90a57
big cock <0424002@nkust.edu.tw>
2021-02-04 21:16:00 +0800

    新增api和之前的api修正

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('date', views.list_of_stock_by_date),
+    path('date', views.list_of_stock_by_date, name="search_by_date"),
+    path('name', views.stock_by_name, name="search_by_name_and_date"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.http import JsonResponse
 from crawler_app.models import PCHStock
-import time
 from datetime import datetime
 from django.views.decorators.csrf import csrf_exempt
 import json
@@ -11,29 +10,62 @@ def list_of_stock_by_date(request):
     try:
         if request.method == 'POST':
             data = json.loads(request.body.decode('utf-8'))
-            # return JsonResponse({"data": data})
             if 'search_date' in data:
-                search_date = data['search_date']
-                search_date = datetime.strptime(search_date.split(' ')[0], '%Y/%m/%d')
-                start_datetime = search_date.replace(hour=0, minute=0, second=0)
-                end_datetime = search_date.replace(hour=23, minute=59, second=59)
-                list_stock = PCHStock.objects.filter(
-                    create_datetime__gte=start_datetime,
-                    create_datetime__lte=end_datetime
-                )
-                result_data = []
-                for item in list_stock:
-                    temp_obj = {}
-                    temp_obj['stock_name'] = item.stock_name
-                    temp_obj['buy_price'] = item.buy_price
-                    temp_obj['sell_price'] = item.sell_price
-                    temp_obj['average_buy_price'] = item.average_buy_price
-                    temp_obj['average_sell_price'] = item.average_sell_price
-                    temp_obj['net_buy'] = item.net_buy
-                    temp_obj['stock_date'] = item.stock_date
-                    result_data.append(temp_obj)
-                return JsonResponse({
-                    "data": result_data,
-                })
+                items = PCHStock.objects.all()
+                list_stock = list_filter_by_date(items, data['search_date'])
+                return parsed_list(list_stock)
+            else:
+                return none_response()
     except Exception as e:
         raise e
+
+
+@csrf_exempt
+def stock_by_name(request):
+    try:
+        if request.method == 'POST':
+            data = json.loads(request.body.decode('utf-8'))
+            if 'search_name' in data:
+                items = PCHStock.objects.filter(stock_name=data['search_name'])
+                if 'search_date' in data:
+                    items = list_filter_by_date(items, data['search_date'])
+                return parsed_list(items)
+            else:
+                return none_response()
+    except Exception as e:
+        raise e
+
+
+def list_filter_by_date(items, search_date):
+    search_date = datetime.strptime(search_date.split(' ')[0], '%Y/%m/%d')
+    start_datetime = search_date.replace(hour=0, minute=0, second=0)
+    end_datetime = search_date.replace(hour=23, minute=59, second=59)
+    list_stock = items.filter(
+        create_datetime__gte=start_datetime,
+        create_datetime__lte=end_datetime
+    )
+    return list_stock
+
+
+def parsed_list(list_stock):
+    result_data = []
+    for item in list_stock:
+        temp_obj = {
+            'stock_name': item.stock_name,
+            'buy_price': item.buy_price,
+            'sell_price': item.sell_price,
+            'average_buy_price': item.average_buy_price,
+            'average_sell_price': item.average_sell_price,
+            'net_buy': item.net_buy,
+            'stock_date': item.stock_date
+        }
+        result_data.append(temp_obj)
+    return JsonResponse({
+        "data": result_data,
+    })
+
+
+def none_response():
+    return JsonResponse({
+        "data": []
+    })
